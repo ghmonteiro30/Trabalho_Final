6a1a9de2e92c0faeea2cf9b7e9c20868b0a3b9ed
CardosoNascimento <cardoson52@gmail.com>
2026-01-10 16:38:36 -0300

    Comentário dos nomes e matriculas

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/* Trabalho Final – Banco da Cochinchina do Sul - Disciplina Programação (CK0226) 
+   Autores: ANTONIO CARDOSO DO NASCIMENTO NETO - 508199
+			FRANCISCO DAVID VAZ DE SOUSA - 554152
+			GABRIEL HENRIQUE MONTEIRO DE MORAIS - 548113
+
+   Data: Janeiro de 2026
+   Descrição: O programa começa recebendo um número x de 1 a 9999, que será usado para identificar
+   o arquivo de entrada e nomear o arquivo de saida no formato entrada-X.txt e saida-X.txt, respectivamente.
+   O programa então executa a simulação de atendimento com os dados recebidos na entrada e gera a saída
+
+   CONTRIBUIÇÕES DOS AUTORES:
+	- ANTONIO CARDOSO DO NASCIMENTO NETO: Implementação do escalonador
+
+	- FRANCISCO DAVID VAZ DE SOUSA: Implementação do registrador
+
+	- GABRIEL HENRIQUE MONTEIRO DE MORAIS: Implementação da fila
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "fila_fifo.h"
